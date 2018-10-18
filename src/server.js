77ff6dc67a6edaa71c717620350df0336eba0a7a
Roman Nikrmajer <roman.nikrmajer@brandembassy.com>
2018-10-18 12:20:18 +0200

    Added standard lint support

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ dotenv.config()
 
 checkDotEnv(process.env)
 
-const {PORT, REDIS_HOST, REDIS_PORT} = process.env
+const { PORT, REDIS_HOST, REDIS_PORT } = process.env
 const ioServer = io(PORT)
-ioServer.adapter(redisAdapter({ host: REDIS_HOST, port: REDIS_PORT }));
+ioServer.adapter(redisAdapter({ host: REDIS_HOST, port: REDIS_PORT }))
